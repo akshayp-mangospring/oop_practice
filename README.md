37e81ea6943541584aa5321ede83dbeb659c137e
akshayp-mangospring <akshayp@mangospring.com>
2022-03-04 21:26:18 +0530

    Add base for Ruby, Java & Python

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+### To run programs in Java
+> ```cd java```
+> ```javac ./Main.java```
+> ```java Main```
+
+### To run programs in Ruby
+> ```cd ruby```
+> ```ruby ./main.rb```
+
 ### To run programs in Python 2
->  python ./main.py
+> ```cd python```
+> ```python ./main.py```
+
 ### To run programs in Python 3
->  python3 ./main.py
+> ```cd python```
+> ```python3 ./main.py```
--- python/main.py
+++ python/main.py
@@ -1,15 +1,7 @@
-item1 = "Phone"
-item1_price = 100
-item1_qty = 5
-item1_total_cost = item1_price * item1_qty
-
 # Fun facts:
 # Python is implemented in C, since it is an interpreted language
 # Even Ruby & JS are implemented in C/C++, they are interpreted languages too.
 # But unlike JS; Ruby & Python have strict typing.
 # Ruby & Python are very similar.
 
-print(type(item1)) # str
-print(type(item1_price)) # int
-print(type(item1_qty)) # int
-print(type(item1_total_cost)) # int
+print "Hello, World!"
